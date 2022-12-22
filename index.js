1c6d98660bd6e2574cf6bac75c94f2dc40a1c01e
Anup chavan <anupm562@gmail.com>
2022-12-22 13:13:50 +0530

    third  commit

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const CustomerRoute = require('./Routes/CustomerRoute')
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path=require('path')
+const dirPath=path.join(__dirname,'/public/build')
 require('dotenv').config();
 
 const app = express();
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public/build'))
+app.use(express.static(dirPath))
 mongoose.set("strictQuery", false);
 
 
